df60e59420df65c2d6ba20fa850f0417bbe75010
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-18 12:28:15 +0530

    Added Hello Component

---
+++ src/components/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div>React - Getting Started by Samer Buna</div>
+    )
+}
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './components/Hello';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
