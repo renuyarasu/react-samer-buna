ae552f21c556c92f469105c61c7a8e0451b174e7
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-21 12:04:48 +0530

    Math.random on arrow function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-function result() {
-    console.log(Math.random());
-}
 
 function Button() {
     const [counter, setCounter] = useState(0);
     return (
-        <button onClick={result}>{counter}</button>
+        <button onClick={() => console.log(Math.random())}>{counter}</button>
     )
 }
 export default Button;
