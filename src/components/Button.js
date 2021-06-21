cb428093b7b6564ccca2f0cd6db12c91ab73548a
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-21 12:16:02 +0530

    counter increment +5

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,6 @@ import React, { useState } from 'react';
 
 function Button() {
     const [counter, setCounter] = useState(0);
-    return <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+    return <button onClick={() => setCounter(counter + 5)}>{counter}</button>
 }
 export default Button;
