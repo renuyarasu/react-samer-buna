14b6293314f64f24dedf28c6da0960f2a4c1bc9d
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-22 12:29:09 +0530

    Working with testData wth .map()

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,13 +5,11 @@ const testData = [
     { name: "Dan Abramov", avatar_url: "https://avatars.githubusercontent.com/u/1", company: "innRoad" },
     { name: "Sophie Alpert", avatar_url: "https://avatars.githubusercontent.com/u/3", company: "innRoad" },
     { name: "Sebastian Markbåge", avatar_url: "https://avatars.githubusercontent.com/u/7", company: "Facebook" },
-  ];
-  
+];
+
 const CardList = (props) => (
     <div>
-        <Card {...testData[0]}/>
-        <Card {...testData[1]}/>
-        <Card {...testData[2]}/>
+        {testData.map(profile => <Card {...profile} />)}
     </div>
 )
 export default CardList;
